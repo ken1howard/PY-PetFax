2916844d1b6017713b414d1465fde13f210bec8e
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-08-13 08:10:09 -0700

    first commit

---
+++ petfax/pet.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+import json
+
+bp = Blueprint('pet', __name__, url_prefix="/pets")
+
+@bp.route('/')
+def index():
+    pets = json.load(open('pets.json'))
+    return render_template('index.html', pets=pets)
